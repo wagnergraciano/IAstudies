8512ee2205af94190b8afb496359e209cd3f4eed
bed72 <gabriel.jr@aluno.ifsc.edu.br>
2020-11-25 22:11:29 -0300

    :tada: ajustes

--- app/api/v1/routers.py
+++ app/api/v1/routers.py
@@ -6,11 +6,17 @@ from fastapi import APIRouter, Depends, HTTPException, status
 router = APIRouter()
 
 
-@router.post("/greedy_search")
-def greedy_search():
+@router.post("/greedy_search", response_model=schemas.AlgorithmsResponseSchemas)
+def greedy_search(*, product_in: schemas.GreedySearchSchemas) -> Any:
+    '''
+        Pegar dados e passar para o Schema referente ao algoritmo Busca Gulosa
+    '''
     return {"Hello": "World"}
 
 
-@router.post("/a_star")
-def a_star():
+@router.post("/a_star", response_model=schemas.AlgorithmsResponseSchemas)
+def a_star(*, product_in: schemas.AStarSchemas) -> Any:
+    '''
+        Pegar dados e passar para o Schema referente ao algoritmo A*
+    '''
     return {"Hello": "World"}
