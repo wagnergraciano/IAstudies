c784f7a6a7380ea9043bf9455b758fb202542e39
José Gilberto <medeiros.gilberto.br@gmail.com>
2020-12-16 01:44:33 -0300

    Creating greedy search class

--- app/api/v1/routers.py
+++ app/api/v1/routers.py
@@ -24,11 +24,11 @@ grid: list = [
 ]
 
 robots: dict = {
-    'R0': (12, 0),
-    'R1': (12, 1),
-    'R2': (12, 2),
-    'R3': (12, 3),
-    'R4': (12, 4)
+    'r0': (12, 0),
+    'r1': (12, 1),
+    'r2': (12, 2),
+    'r3': (12, 3),
+    'r4': (12, 4)
 }
 
 @router.post("/greedy_search")
@@ -36,7 +36,8 @@ async def greedy_search(item: schemas.GreedySearchSchemas) -> list:
     '''
         Pegar dados e passar para o Schema referente ao algoritmo Busca Gulosa
     '''
-    return {}
+    result = GreedySearch().search(item.package)
+    return result
 
 
 @router.post("/a_star")
@@ -44,7 +45,6 @@ async def a_star(item: schemas.AStarSchemas) -> list:
     '''
         Pegar dados e passar para o Schema referente ao algoritmo A*
     '''
-
     result = AStar().search(item.package)
     return result
 
@@ -183,7 +183,123 @@ class AStar:
         }
 
 class GreedySearch:
+
+    def __init__(self):
+        self.path = []
+
+    def view_content(self, i, j) -> int:
+        """Visualiza o conteúdo de uma posição qualquer da matriz/grid.
+        """
+        return grid[i][j]
+
+    def choose_best_robot(self, start) -> str:
+        """ Definido um start para procurar o robô, essa função calcula com base
+            na heuristica, Manhattan distance, qual o robô mais próximo do start e retorna
+            sua chave no dict de robôs da aplicação.
+        """
+        costs = {}
+        for robot in robots.keys():
+            costs[robot] = self.heuristic(robots[robot], start)
+        
+        return min(costs.items(), key=lambda x: x[1])[0]
+
+    def find_real_start(self, start) -> tuple:
+        """ Define a posição inicial para busca, considerando um (i, j) pertecente a 
+            um corredor qualquer mais próximo do (i_0, j_0) da posição inicial.
+        """
+
+        # Verifica a direita
+        if (start[1] + 1 < len(grid[0])) and (self.view_content(start[0], start[1] + 1) not in [0, 2, 4]):
+            return (start[0], start[1] + 1)
+        # Verifica a esquerda
+        if (start[1] - 1) >= 0 and (self.view_content(start[0], start[1] - 1) not in [0, 2, 4]):
+            return (start[0], start[1] - 1)
+
+    def heuristic(self, cord0, cord1) -> int:
+        """Implementa uma heuristica com base na distância de manhattan.
+        """
+        return abs(cord0[0] - cord1[0]) + abs(cord0[1] - cord1[1])
+
+    def expand_nodes(self, node: Node):
+        # se o conteudo da célula atual for um robo achou
+        if self.view_content(node.position[0], node.position[1]) == 3:
+            # path.append('{}:{}'.format(node.position[0], node.position[1]))
+            return
+
+        expand_node = []
+
+        # caso contrario expande para as direcoes possiveis
+
+        # Verifica 1 passo para baixo
+        if (node.position[0] - 1) >= 0 and (self.view_content(node.position[0] - 1, node.position[1]) not in [0, 2, 4]):
+
+            # verifica se é um ciclo (neto = avô)
+            if node.parent == None or (node.position[0] - 1, node.position[1]) != node.parent.position:
+                expand_node.append(
+                    Node(node,
+                        (node.position[0] - 1, node.position[1]),
+                        self.heuristic(
+                            (node.position[0] - 1, node.position[1]), robots[node.robot]),
+                        node.robot,
+                        0))
+
+        # Verifica 1 passo para cima
+        if (node.position[0] + 1) < len(grid) and (self.view_content(node.position[0] + 1, node.position[1]) not in [0, 2, 4]):
+            if node.parent == None or (node.position[0] + 1, node.position[1]) != node.parent.position:
+                expand_node.append(
+                    Node(node,
+                        (node.position[0] + 1, node.position[1]),
+                        self.heuristic(
+                            (node.position[0] + 1, node.position[1]), robots[node.robot]),
+                        node.robot,
+                        0))
+
+        # Verifica 1 passo para esquerda
+        if (node.position[1] - 1 >= 0) and (self.view_content(node.position[0], node.position[1] - 1) not in [0, 2, 4]):
+            if node.parent == None or (node.position[0], node.position[1] - 1) != node.parent.position:
+                expand_node.append(
+                    Node(node,
+                        (node.position[0], node.position[1] - 1),
+                        self.heuristic(
+                            (node.position[0], node.position[1] - 1), robots[node.robot]),
+                        node.robot,
+                        0))
+
+        # Verifica 1 passo para direita
+        if (node.position[1] + 1 < len(grid[0])) and (self.view_content(node.position[0], node.position[1] + 1) not in [0, 2, 4]):
+            if node.parent == None or (node.position[0], node.position[1] + 1) != node.parent.position:
+                expand_node.append(Node(
+                    node,
+                    (node.position[0], node.position[1] + 1),
+                    self.heuristic(
+                        (node.position[0], node.position[1] + 1), robots[node.robot]),
+                    node.robot,
+                    0
+                ))
+
+        node_to_expand = sorted(expand_node, key = lambda x: x.cost)[0]
+
+        # print('Nós expandidos: {}'.format(expand_node))
+        # print('Nó escolhido para expandir: {}'.format(node_to_expand))
+        
+        self.path.append('{}:{}'.format(node_to_expand.position[0], node_to_expand.position[1]))
+        self.expand_nodes(node_to_expand)
     
     def search(self, start) -> list:
-        pass
+        real_start = self.find_real_start(start)
+        robot = self.choose_best_robot(real_start)
 
+        self.path.append('{}:{}'.format(start[0], start[1]))
+        self.path.append('{}:{}'.format(real_start[0], real_start[1]))
+
+        node = Node(None, real_start, 0, robot, 0)
+        self.expand_nodes(node)
+        
+        grid[robots[robot][0]][robots[robot][1]] = 1
+        grid[start[0]][start[1]] = 3
+        robots[robot] = tuple(start)
+
+        return {
+            'result': self.path[::-1],
+            'robot': robot
+        }
